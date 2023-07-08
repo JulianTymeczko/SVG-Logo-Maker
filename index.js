e43f7fe6ef6ca9049570e55b2ed549ec2a67ca64
Julian Houle-T <jhouletymeczko@gmail.com>
2023-07-08 07:22:25 -0400

    finally creates svg file looking to meet all other requirements

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
+const createSVG = require('./util/createSVG')
 inquirer
   .prompt([
     {
       type: 'input',
       message: 'What text colour would you like?',
-      name: 'Text-colour',
+      name: 'textColor',
     },
     {
       type: 'list',
       message: 'What shape would you like?',
-      name: 'Shape',
+      name: 'shape',
       choices: ['circle', 'triangle', 'square'],
     },
     {
       type: 'input',
       message: 'What is the colour of the shape you would like?',
-      name: 'Shape-colour',
+      name: 'shapeColor',
     },
   ])
-  .then((response) =>
-    console.log(response)
-     
+  .then((response) => { 
+    
+    const { textColor, shape, shapeColor } = response
+    const SVG = createSVG(textColor, shape, shapeColor)
+    const filename = `logo.svg`
+    fs.writeFile(filename, SVG, (err) =>
+    err ? console.log(err) : console.log("Generated logo.svg"))
+   console.log(response)
+  }
   );
---
+++ util/createSVG.js
@@ -0,0 +1,17 @@
+
+
+function createSVG(textColor, shape, shapeColor) {
+    return `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <${shape} cx="150" cy="100" r="80" fill="${shapeColor}" />
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">SVG</text>
+  
+  </svg>
+    `;
+  }
+
+
+module.exports = createSVG  
+  
